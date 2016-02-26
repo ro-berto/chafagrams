d67106627209c0484af8f2aeb0db3421d4fa6e1f
Roberto Carrillo <robertocn@google.com>
2016-02-25 17:34:16 -0800

    Persistence and recents page

--- chafagram.py
+++ chafagram.py
@@ -1,15 +1,33 @@
+import json
 import uuid
 
+from google.appengine.ext import ndb
 from PIL import Image
+
 import cloudstorage as gcs
 import webapp2
 
+
 BUCKET = 'chafagramdemophotos'
+
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Hello, World!')  # TODO: Serve app here.
+    self.response.headers['Content-Type'] = 'text/html'
+    self.response.write(open(
+        os.path.join(os.path.dirname(__file_), 'static','shell.html')).read())
+
+
+class RecentPage(webapp2.RequestHandler):
+  def get(self):
+    result = []
+    for c in Chafagram.query().order(-Chafagram.date).fetch(20):
+      result.append({'url': c.image_file,
+                     'date': c.date,
+                     'comment': c.comment})
+    self.response.headers['Content-Type'] = 'application/json'
+    self.response.write(json.dumps(result))
+
 
 class PutPage(webapp2.RequestHandler):
   def post(self):
@@ -21,14 +39,28 @@ class PutPage(webapp2.RequestHandler):
     gcspath = '/%s/%s' % (BUCKET, filename)
     write_retry_params = gcs.RetryParams(backoff_factor=1.1)
     gcsfile =  gcs.open(gcspath, 'w', content_type='image/png',
-                        options={'x-goog-meta-comment': comment},
                         retry_params=write_retry_params)
     image.save(gcsfile, 'PNG')
     gcsfile.close()
+
+    chafagram = Chafagram()
+    chafagram.image_file = filename
+    chafagram.comment = comment
+    chafagram.save()
+
     self.response.headers['Content-Type'] = 'text/plain'
     self.response.write('Photo uploaded as: ' + gcspath)
+
+
+class Chafagram(ndb.Model):
+  image_file = ndb.StringProperty()
+  comment = ndb.StringProperty()
+  date = ndb.DateTimeProperty(auto_now_add=True)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/put', PutPage),
+    ('/recent', RecentPage),
 ], debug=True)
+
